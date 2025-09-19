470f1b406ca54d9290dd1847783bd65f67952a05
Bazagod <bazestininon@gmail.com>
2025-09-19 15:11:41 +0200

    Update LinkedIn profile link in ContactSection.vue

--- src/components/ContactSection.vue
+++ src/components/ContactSection.vue
@@ -44,11 +44,7 @@
         </div>
         <div class="flex flex-col justify-center">
           <h4 class="font-semibold">LinkedIn</h4>
-<<<<<<< HEAD
-          <a href="https://www.linkedin.com/in/ton-profil" target="_blank" rel="noopener noreferrer"
-=======
           <a href="https://www.linkedin.com/in/estimé-ninon-bazayo-a827a5329" target="_blank" rel="noopener noreferrer"
->>>>>>> 8b7e48b7a6eadb00bcaabcf4af472a5afaacf7cb
              class="text-gray-300 hover:text-blue-400 transition">
             Estimé Ninon BAZAYO
           </a>
